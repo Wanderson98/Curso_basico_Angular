32e970ac394c58b81925a9c87d710a2923a455af
Wanderson Andrade <wmandrade81@gmail.com>
2022-06-03 22:14:16 -0300

    Começando a ver Eventos

---
+++ src/app/components/events/events.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-events',
+  templateUrl: './events.component.html',
+  styleUrls: ['./events.component.css']
+})
+export class EventsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  show: boolean = false; //booleano
+
+  showMessage():void{
+    this.show = !this.show; //toggle
+  }
+}
