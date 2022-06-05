b6d2937dbfc1a25d5a4004b4f6249103ff07219b
Wanderson Andrade <wmandrade81@gmail.com>
2022-06-05 00:51:56 -0300

    Conceito services

--- src/app/components/list-rener/list-rener.component.ts
+++ src/app/components/list-rener/list-rener.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Animal } from 'src/app/Animal';
+
+import { ListService } from 'src/app/services/list.service';
 @Component({
   selector: 'app-list-rener',
   templateUrl: './list-rener.component.html',
@@ -8,13 +10,15 @@ import { Animal } from 'src/app/Animal';
 })
 export class ListRenerComponent implements OnInit {
 
-  constructor() { }
+  constructor(private listService: ListService) {
+    
+  }
 
   ngOnInit(): void {
   }
 
   showAge = false;
- animalDetails = '';
+  animalDetails = '';
   animals: Animal[] = [
     {name: "Turca", type: "dog", age: 2},
     {name: "Tom", type: "cat",age: 1},
@@ -24,5 +28,9 @@ export class ListRenerComponent implements OnInit {
 
   ShowAge(animal:Animal):void{
   this.animalDetails = `O animal ${animal.name} tem ${animal.age} anos de vida`
+  }
+
+  removeAnimal(animal:Animal){
+     this.animals = this.listService.remove(this.animals, animal);
   }
 }
